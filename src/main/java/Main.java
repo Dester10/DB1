973380c2e60e9482dc8a96e8095674e40e5ad75f
Maxim <MaximSe20@yandex.ru>
2023-12-14 00:14:15 +0300

    First commit bd

---
+++ src/main/java/Main.java
@@ -0,0 +1,25 @@
+package main.java;
+
+public class Main {
+    public static void main(String[] args) {
+        FirstConnect connect = new FirstConnect();
+        String select = """
+                select *
+                from audit_department
+                """;
+
+        String[][] obj = connect.allSelect(select);
+
+        System.out.println("----------------------------------------------------------");
+        System.out.printf("|%-18s|%-18s|%-18s| \n", "Ревизионный номер", "Тип изделия", "Адрес расположения");
+        System.out.println("----------------------------------------------------------");
+
+        for (int i = 0; i < obj.length; ++i) {
+            for (int j = 0; j < obj[0].length; ++j) {
+            }
+
+            System.out.println("| \n");
+            System.out.println("----------------------------------------------------------");
+        }
+    }
+}
---
+++ src/main/resources/create.sql
@@ -0,0 +1,6 @@
+create table audit_department
+(
+    Ревизионный_номер text not null primary key ,
+    Тип_изделия text not null,
+    Адрес_расположения text not null
+);
